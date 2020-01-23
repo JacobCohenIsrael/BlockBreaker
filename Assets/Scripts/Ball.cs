7f5bcd41910c6673b6b66b9ad162c1f95db029ae
jacobbum <jacob@bbumgames.com>
2020-01-23 15:55:26 +0200

    Initial Commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField] public Paddle paddle;
+    [SerializeField] float xPush = 2.0f;
+    [SerializeField] float yPush = 15.0f;
+    [SerializeField] AudioClip[] ballSounds;
+
+    private AudioSource audioSource;
+    private Vector2 paddleToBallVector;
+    public bool hasStarted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+        }
+        LaunchOnMouseClick();
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 velocityTweak = new Vector2(
+                UnityEngine.Random.Range(-2.0f, xPush),
+                UnityEngine.Random.Range(0f, yPush)
+                );
+            GetComponent<Rigidbody2D>().velocity = velocityTweak;
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (hasStarted)
+        {
+            var clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            audioSource.PlayOneShot(clip);
+            if (collision.gameObject.tag != "Ball")
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float horizonalUnits = 16f;
+    [SerializeField] private float maxX = 15f;
+    [SerializeField] private float minX = 1f;
+    [SerializeField] Ball ballPrefab;
+    [SerializeField] Transform ballBarrelPosition;
+
+    void Update()
+    {
+        var mousePositionX = Mathf.Clamp(Input.mousePosition.x / Screen.width * horizonalUnits, minX, maxX);
+        Vector2 paddlePosition = new Vector2(mousePositionX, transform.position.y);
+        transform.position = paddlePosition;
+
+        LaunchBallOnMouseClick();
+    }
+
+    private void LaunchBallOnMouseClick()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var ball = Instantiate<Ball>(ballPrefab, ballBarrelPosition.position, transform.rotation);
+            ball.paddle = this;
+        }
+    }
+}
